b5f9d12640a9aee79ef74154f0e825b2ea25e7c2
mun-sojeong <82130275+mun-sojeong@users.noreply.github.com>
2021-04-08 16:27:38 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello{
+	public static void main(String [] args){
+	 System.out.println("Hello World!");
+	 System.out.println("Mun so jeong");
+	 System.out.println("문소정");
+	
+		for(int a = 0; a< 6; a++){
+
+			for(int b = 0; b < a; b++){	
+			  System.out.print("*");
+		 	}
+
+		System.out.println("");  
+		}
+ 	}
+}
